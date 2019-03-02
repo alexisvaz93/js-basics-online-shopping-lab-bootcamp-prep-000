ac741e167fce64785ca5baf3c59ef1b9e7db44aa
Alexis Vazquez <alexisvaz93@yahoo.com>
2019-03-02 10:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ function viewCart() {
     else if (cart.length > 0){
       for(var i=0; i < cart.length; i++){
         if(i === 0){
-          temp = getCart()[i]["itemPrice"];
           string1 += `${cart[i].itemName} at $${cart[i].itemPrice}`;
         }
         else if (i > 0 && i < cart.length - 1){
